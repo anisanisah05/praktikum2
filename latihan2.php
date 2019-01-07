cdbc0e9c8c36fbc29246b8ade02752a842ba4635
anisah nabilah <anisah nabilah>
2019-01-07 15:08:23 +0700

    first commit

---
+++ latihan2.php
@@ -0,0 +1,16 @@
+<?php
+
+$x='4';
+$y='5';
+
+echo "<h3> hasil operasi menggunakan operator perbandingan </h3>";
+echo "x = $x <br>";
+echo "y = $y <br>";
+if ($x == $y) {
+echo "x sama dengan y";
+}
+elseif ($x != $y) {
+  echo " x tidak sama dengan y";
+}
+
+ ?>
---
+++ latihan6.php
@@ -0,0 +1,13 @@
+<?php
+
+$f=4;
+$k=5;
+
+if ($f == 4 || $k == 9) {
+  echo "<br><h1> data benar</h1>";
+} else {
+  echo "<br><h1> data salah</h1>";
+}
+
+
+ ?>
